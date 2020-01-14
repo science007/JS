3bc44eae3e39beda28e600ecb9f0176ce0d1d289
Eugene <eugene_khval@mail.ru>
2020-01-14 21:20:23 +0300

    Конец первого задания + доп.задания

--- LR/JS/javascript.js
+++ LR/JS/javascript.js
@@ -1,24 +1,40 @@
-console.log("Работает)");
-let title_magazine = prompt("Название Вашего магазина?");
-let budjet = prompt("Ваш бюджет?");
+// console.log("Работает)");
+// let title_magazine = prompt("Название Вашего магазина?");
+// let budjet = prompt("Ваш бюджет?");
 
-console.log(title_magazine);
-console.log(budjet);
+// console.log(title_magazine);
+// console.log(budjet);
 
-let mainList = {
-    budjet_of_shop: budjet,
-    title_shop: title_magazine,
-    shopGoods: arr_shopGoods = [],
-    employers: obj_employers = {},
-    Open: true
-}
+// let mainList = {
+//     budjet_of_shop: budjet,
+//     title_shop: title_magazine,
+//     shopGoods: arr_shopGoods = [],
+//     employers: obj_employers = {},
+//     Open: true
+// }
 
-for(let i = 0; i <= 2; i++){
-    let kol = prompt("Какой тип товаров будем продавать?");
-    arr_shopGoods[i] = kol;
-}
+// for(let i = 0; i <= 2; i++){
+//     let kol = prompt("Какой тип товаров будем продавать?");
+//     arr_shopGoods[i] = kol;
+// }
 
-// console.log(mainList['shopGoods'] + ', ');
+// // console.log(mainList['shopGoods'] + ', ');
 
-let budjet_on_day = budjet / 30;
-alert("Бюджет на день = " + budjet_on_day);
+// let budjet_on_day = budjet / 30;
+// alert("Бюджет на день = " + budjet_on_day);
+
+// let num = 33721;
+// var temp = String(num);
+// // num = String(num);
+// var result = 1;
+// for(let i = 0; i <= temp.length - 1; i++){
+//     let y = +(temp[i]);
+//     result = result * y;
+//     // result = +typeof(result);
+// }
+// console.log(result);
+
+// let pow = Math.pow(result, 3);
+// alert(pow);
+
+/* =========== конец первого задания =============*/
